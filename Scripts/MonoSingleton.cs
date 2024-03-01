1216c1c64a647b0b9961f1cbb54cf8e049557d54
zenas <zenas@quadratron.com>
2024-03-01 14:44:47 -0500

    initial commit

---
+++ Scripts/MonoSingleton.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+namespace ZenTools.Custonidan
+{
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        private static object _lock = new object();
+        private static bool applicationIsQuitting = false;
+
+        [SerializeField]
+        private bool persistAcrossScenes = false;
+
+        public static T Instance
+        {
+            get
+            {
+                if (applicationIsQuitting)
+                {
+                    Debug.LogWarning($"[Singleton] Instance '{typeof(T)}' already destroyed. Returning null.");
+                    return null;
+                }
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = (T)FindObjectOfType(typeof(T));
+
+                        if (FindObjectsOfType(typeof(T)).Length > 1)
+                        {
+                            Debug.LogError($"[Singleton] Something went wrong - there should never be more than 1 singleton! Reopening the scene might fix it.");
+                            return _instance;
+                        }
+
+                        if (_instance == null)
+                        {
+                            GameObject singletonObject = new GameObject();
+                            _instance = singletonObject.AddComponent<T>();
+                            singletonObject.name = $"{typeof(T).ToString()} (Singleton)";
+
+                            if (_instance.GetComponent<MonoSingleton<T>>().persistAcrossScenes)
+                            {
+                                DontDestroyOnLoad(singletonObject);
+                            }
+                        }
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                if (persistAcrossScenes)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                applicationIsQuitting = true;
+            }
+        }
+    }
+}
